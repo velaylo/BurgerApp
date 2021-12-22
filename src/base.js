cfd4c7fcb87a0873eea55feaf945d6ba3a40792e
velaylo <yankovich.olga24@gmail.com>
2021-12-23 01:08:13 +0300

    Setting up a firebase database

---
+++ src/base.js
@@ -0,0 +1,13 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyCXsvr3TDwsDGjIL8IN9hPMWO__AmF-gp4",
+    authDomain: "very-hot-burgers-8d8cf.firebaseapp.com",
+    databaseURL: "https://very-hot-burgers-8d8cf-default-rtdb.firebaseio.com",
+})
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebaseApp };
+export default base;
